ad1ffa2d2de832214fbe5957cf650d3df1aba659
Jaime Sazo <estuardosazo1206@gmail.com>
2020-04-28 14:33:00 -0600

    Modificacion

--- index.php
+++ index.php
@@ -4,9 +4,30 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Rest APP</title>
+    <link rel="stylesheet" href="include/css/bootstrap.min.css">
+
+    <style>
+        .abs-center {
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                min-height: 100vh;
+                }
+               
+    </style>
 </head>
 <body>
-<h1>Hola mundo!</h1>
+
+
+<div class="container">
+  <div class="abs-center">
+    <h1>RestAPP</h1>
+  </div>
+</div>
     
+
+    <!-- Librerias script que se utilizaran -->
+    <script src="include/js/jquery3.5.min.js"></script>
+    <script src="include/js/bootstrap.min.js"></script>
 </body>
 </html>
