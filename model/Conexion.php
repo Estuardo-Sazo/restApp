b38bc25056fa609ea39cca09493108761d80825c
Jaime Sazo <estuardosazo1206@gmail.com>
2020-04-28 14:33:24 -0600

    Modificacion total

---
+++ model/Conexion.php
@@ -0,0 +1,22 @@
+<?php
+
+    require 'Parametros.php';
+    class Conexion{
+        protected $con;
+
+        function __construct(){
+              $this->con=new mysqli(SERVER,USER,PASSWORD,DB);
+             // $this->con->set_charset("CHAR");
+        }
+
+        
+    
+    }
+
+
+  
+
+
+?>
+
+
---
+++ model/Parametros.php
@@ -0,0 +1,11 @@
+<?php
+define("SERVER","localhost");
+define("USER","root");
+define("PASSWORD","");
+define("DB","database_1_charcas");
+define("CHAR","utf-8");
+
+
+
+
+?>
