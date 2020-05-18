39ff625514c48fad36e6f0bea68b22f96d58a624
Jaime Sazo <estuardosazo1206@gmail.com>
2020-05-18 11:24:14 -0600

    Update main.js

--- model/login/js/main.js
+++ model/login/js/main.js
@@ -9,15 +9,28 @@ $(document).ready(function (){
 
         $.post('php/controlUser.php',datos,function(response){
             let Datos = JSON.parse(response);
-            Datos.forEach(d => {
-                $('#txtUser').val('');
-                $('#txtPass').val('');
-                $('#Nombre').html(d.usu_nombre+' '+d.usu_apellido);
-                $('#load').html(`<div class="spinner-grow text-light" role="status">
-                <span class="sr-only">Loading...</span>
-                </div>`);
-            });
-            Inicio();
+            
+            if(Datos.length>0){
+                Datos.forEach(d => {
+                    $('#txtUser').val('');
+                    $('#txtPass').val('');
+                    $('#Nombre').html(d.usu_nombre+' '+d.usu_apellido);
+                    $('#load').html(`<div class="spinner-grow text-light" role="status">
+                    <span class="sr-only">Loading...</span>
+                    </div>`);
+                });
+                Inicio();
+            }else{
+                $('#alert1').html(`
+                <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                <strong>ERROR!</strong> No hay considencia con los datos ingresados.
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+                `);
+            }
+            
             
             console.log(Datos);
         });
