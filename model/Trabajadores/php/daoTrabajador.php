0daac462eb573a07407d545290fe29ae4e97730a
Jaime Sazo <estuardosazo1206@gmail.com>
2020-05-18 11:26:09 -0600

    Update daoTrabajador.php

--- model/Trabajadores/php/daoTrabajador.php
+++ model/Trabajadores/php/daoTrabajador.php
@@ -22,12 +22,27 @@
             return $res;  
          }
 
+         function actualizarTrab($id,$nombre,$pago,$idT)
+         {
+            $res=$this->con->query("UPDATE tbl_trabajadores SET trab_nombre='$nombre',trab_pago=$pago,tptra_id=$idT WHERE trab_id=$id");
+            
+            return $res;  
+         }
+
          function buscarTrab($idR)
          {
             $res=$this->con->query("SELECT tb.trab_id,tb.trab_nombre,tb.trab_pago,tp.tb_trabajo FROM tbl_trabajadores tb ,tbl_tipo_tra tp WHERE tp.rest_id='$idR' AND  tp.tb_id=tb.tptra_id");
             
             return $res;  
          }
+
+         function buscarTrab2($idT)
+         {
+            $res=$this->con->query("SELECT * FROM tbl_trabajadores  WHERE trab_id=$idT");
+            
+            return $res;  
+         }
+
          function elimiTrab($idT)
          {
             $res=$this->con->query("DELETE FROM tbl_trabajadores  WHERE trab_id=$idT ");
